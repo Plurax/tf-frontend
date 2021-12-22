1d77ece798d52ac053dde6ae96bd2667325440b5
Christoph Uhlich <christoph@familie-uhlich.de>
2021-12-22 16:55:13 +0100

    Button style fix.

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+<div class="rounded px-8 pt-6 pb-8 mb-4 col-span-8 flex justify-center">
  <form @submit.prevent="userLogin">
     <div class="mb-4">
       <label class="block text-grey-darker text-sm font-bold mb-2" for="email">
