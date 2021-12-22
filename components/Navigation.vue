696e1846bc95af46b07d1651cdb27ed561672a22
Christoph Uhlich <christoph@familie-uhlich.de>
2021-12-22 16:54:19 +0100

    Add Nav for login. Remove grouping.

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -30,11 +30,11 @@
       </div>
       <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
         <div class="hidden sm:block sm:ml-6">
-          <div v-if="loggedInUser" class="flex space-x-4">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <NuxtLink to="/tasks" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Aufgaben</NuxtLink>
-            <NuxtLink to="/tasks/create" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Neue Aufgabe</NuxtLink>
-          </div>
+          <NuxtLink to="/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</NuxtLink>
+          <NuxtLink v-if="!loggedInUser" to="/login" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Login</NuxtLink>
+          <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+          <NuxtLink v-if="loggedInUser" to="/tasks" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Aufgaben</NuxtLink>
+          <NuxtLink v-if="loggedInUser" to="/tasks/create" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Neue Aufgabe</NuxtLink>
         </div>
       </div>
       <div v-if="loggedInUser" class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -83,7 +83,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(["isAuthenticated", "loggedInUser"]),
+    ...mapGetters(["loggedInUser"]),
   },
   methods: {
     async logout() {
