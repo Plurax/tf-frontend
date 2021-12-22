10fb06336aad4bc538639cfc6f825999768eb2bd
Christoph Uhlich <christoph@familie-uhlich.de>
2021-12-22 16:55:58 +0100

    Remove unused storage items.

--- store/index.js
+++ store/index.js
@@ -1,11 +1,4 @@
 export const getters = {
-  isAuthenticated(state) {
-    return state.auth.loggedIn;
-  },
-
-  rawJwt(state) {
-    return substring(7, state.auth._token.local); 
-  },
   loggedInUser(state) {
     return state.auth.user;
   },
